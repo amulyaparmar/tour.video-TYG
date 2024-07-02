7bef0a252c1de555bcfabae3aa14c77f80e6540c
Amulya Parmar <parmar.amulya@gmail.com>
2024-07-02 18:30:12 +0000

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,45 @@
+"use client";
+import { useRouter } from "next/navigation";
+import Head from "next/head";
+
+export default function Home() {
+  const router = useRouter();
+  const { alias, ...urlParams } = router.query;
+
+  // Construct the iframe src URL
+  const baseUrl = "https://embed.tour.video";
+  const queryString = new URLSearchParams({
+    uuid: alias,
+    ...urlParams,
+  }).toString();
+  const iframeSrc = `${baseUrl}?${queryString}`;
+
+  return (
+    <>
+      <Head>
+        <title>{`Video Tour for ${alias}`}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <iframe
+        src={iframeSrc}
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          border: "none",
+        }}
+        allow="camera *; microphone *; autoplay *; encrypted-media *; fullscreen *; display-capture *; clipboard-write"
+        allowFullScreen
+      />
+    </>
+  );
+}
+
+// This ensures that the page is rendered on the server side
+// export async function getServerSideProps(context) {
+//   return {
+//     props: {}, // will be passed to the page component as props
+//   };
+// }
---
+++ app/v2/[alias]/DynamicIframe.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export default function DynamicIframe({ alias, initialSearchParams }) {
+  const searchParams = useSearchParams();
+  const [iframeSrc, setIframeSrc] = useState("");
+
+  useEffect(() => {
+    const baseUrl = "https://new-videomagnet.onrender.com";
+    const queryString = new URLSearchParams({
+      uuid: alias,
+      ...Object.fromEntries(searchParams.entries()),
+    }).toString();
+    setIframeSrc(`${baseUrl}?${queryString}`);
+  }, [alias, searchParams]);
+
+  if (!iframeSrc) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <iframe
+      src={iframeSrc}
+      style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+        border: "none",
+      }}
+      allow="camera *; microphone *; autoplay *; encrypted-media *; fullscreen *; display-capture *; clipboard-write"
+      allowFullScreen
+    />
+  );
+}
