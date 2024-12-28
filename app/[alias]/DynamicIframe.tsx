512a5fec22b7286ad8cf443ebdc5abd42ac8e589
Kingdom Kid - Amulya Parmar <amulya@umich.edu>
2024-12-28 11:50:07 -0500

    Update the new videomagnet on render link TYG

--- app/[alias]/DynamicIframe.tsx
+++ app/[alias]/DynamicIframe.tsx
@@ -8,7 +8,7 @@ export default function DynamicIframe({ alias='@vue32', initialSearchParams }) {
   const [iframeSrc, setIframeSrc] = useState("");
 
   useEffect(() => {
-    const baseUrl = "https://new-videomagnet.onrender.com";
+    const baseUrl = "https://new-videomagnet-658y.onrender.com";
     console.log("aliasTYG: ", alias);
     const queryString = new URLSearchParams({
       uuid: alias.replace("%40", "@"), // for some reason the url stringifies '@' to '%40' so I am just replacing it to pass in the data TYG
