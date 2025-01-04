bafbfb102e4584f09927af48ff23fdbcb200af36
Muhammad Ahad <muhmmadahad594@gmail.com>
2025-01-04 10:58:38 +0500

    new layout component for nest dashboard file

---
+++ app/dashboard/[community]/[magnet]/layout.tsx
@@ -0,0 +1,43 @@
+"use client";
+import { useVisitorStore } from "@/store/useVisitorStore";
+import { initializeSupabaseRealtime } from "@/utils/supabase";
+import { useEffect, useState } from "react";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  const [channelref, setchannelref] = useState<any>();
+  const { activevisitors, handleJoin, handleLeave, setactivevisitors } = useVisitorStore();
+
+  useEffect(() => {
+    const url = window.location.href;
+  
+    // Regex to match UUIDs
+    const regex = /([a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/;
+    const match = url.match(regex);
+  
+    const magnet_uuid = match ? match[1] : null;
+    console.log("magnet_uuid",magnet_uuid)
+    if (magnet_uuid) {
+      initializeSupabaseRealtime(setchannelref, handleJoin, handleLeave, magnet_uuid);
+    }
+  
+    // Return a cleanup function to unsubscribe when the magnetUuid changes or the component unmounts
+    return () => {
+      if (channelref) {
+        channelref.unsubscribe();
+        setactivevisitors(null);
+        console.log(`Unsubscribed from channel: ${magnet_uuid}`);
+      }
+    };
+  }, []);
+  
+
+  console.log("activevisitors", activevisitors);
+
+  return (
+    <>
+        <main>
+          {children}
+        </main>
+    </>
+  );
+}
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,39 +1,8 @@
 "use client";
 import { AppSidebar } from "@/components/app-sidebar";
 import { SidebarProvider } from "@/components/ui/sidebar";
-import { useVisitorStore } from "@/store/useVisitorStore";
-import { initializeSupabaseRealtime } from "@/utils/supabase";
-import { useEffect, useState } from "react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  const [channelref, setchannelref] = useState<any>();
-  const { activevisitors, handleJoin, handleLeave, setactivevisitors } = useVisitorStore();
-
-  useEffect(() => {
-    const url = window.location.href;
-  
-    // Regex to match UUIDs
-    const regex = /([a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/;
-    const match = url.match(regex);
-  
-    const magnet_uuid = match ? match[1] : null;
-    console.log("magnet_uuid",magnet_uuid)
-    if (magnet_uuid) {
-      initializeSupabaseRealtime(setchannelref, handleJoin, handleLeave, magnet_uuid);
-    }
-  
-    // Return a cleanup function to unsubscribe when the magnetUuid changes or the component unmounts
-    return () => {
-      if (channelref) {
-        channelref.unsubscribe();
-        setactivevisitors(null);
-        console.log(`Unsubscribed from channel: ${magnet_uuid}`);
-      }
-    };
-  }, []);
-  
-
-  console.log("activevisitors", activevisitors);
 
   return (
     <>
