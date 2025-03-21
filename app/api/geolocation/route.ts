311adc59cd573958b23b4f0926a0147fbe33665b
Amulya Parmar! TYG!! <parmar.amulya@gmail.com>
2025-03-21 14:53:22 -0400

    Update the location ip address logic TYG

--- app/api/geolocation/route.ts
+++ app/api/geolocation/route.ts
@@ -4,18 +4,21 @@ import type { NextRequest } from 'next/server'
 
 export async function GET(request: NextRequest) {
   // Get geolocation and IP data directly using Vercel Functions
-  const { city,  country, flag, region, countryRegion, latitude, longitude, postalCode } = geolocation(request)
+  const { city, country, region, countryRegion, latitude, longitude } = geolocation(request)
   const ip = ipAddress(request)
   
   return NextResponse.json({
-    city: city ?? 'unknown',
-    country: country ?? 'unknown',
-    flag: flag ?? 'unknown',
-    region: region ?? 'unknown',
-    countryRegion: countryRegion ?? 'unknown',
-    latitude: latitude ?? 'unknown',
-    longitude: longitude ?? 'unknown',
-    postalCode: postalCode ?? 'unknown',
-    ip: ip ?? 'unknown'
+    error: null,
+    ip: ip ?? 'unknown',
+    location: {
+      accuracy_radius: 5, // Assuming a default value, adjust as needed
+      city: city ?? 'unknown',
+      country: country ?? 'unknown',
+      latitude: latitude ?? 'unknown',
+      longitude: longitude ?? 'unknown',
+      state: countryRegion ?? 'unknown',
+      regionId: region ?? 'unknown'
+    },
+    status: 'success'
   })
 } 
